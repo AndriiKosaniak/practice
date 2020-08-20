aacb7f1353c98cb386d81bf25ad4557818a2760b
AndriiKosanyak <andriikosanyak4@gmail.com>
2020-08-20 21:47:30 +0300

    Update App.vue

--- vuetodolist/App.vue
+++ vuetodolist/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    {{ msg }}
-    <input type="text" v-model="text">
-    <button @click="add" v-if="arr.length <= 10">Add</button>
-    <button @click="remove(i)">Remove</button>
+ <div id="app">
+    <h1>To Do List:</h1>
     <ol>
       <li v-for="(item, i) in arr" :key="i">
         <span>{{item}}</span>
       </li>
     </ol>
+    <input type="text" v-model="text">
+    <button @click="add" v-if="arr.length <= 10">Add</button>
+    <button @click="remove(i)">Remove</button>
   </div>
 </template>
 
